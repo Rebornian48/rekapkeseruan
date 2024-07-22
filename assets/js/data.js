3a7d07b359d2388bc8f013158e71a796c8647750
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-07-22 15:59:01 +0700

    update #1

---
+++ assets/js/data.js
@@ -0,0 +1,46 @@
+new Chart(document.getElementById("myChart"), {
+  type: "bar",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    //, "July", "August", "September", "October", "November", "December"
+    datasets: [
+      {
+        label: "Pekan 1",
+        data: [6, 16, 9, 13, 24, 15, 13],
+      },
+      {
+        label: "Pekan 2",
+        data: [10, 5, 11, 19, 22, 14, 18],
+      },
+      {
+        label: "Pekan 3",
+        data: [8, 7, 15, 21, 33, 20, 15],
+      },
+      {
+        label: "Pekan 4",
+        data: [8, 8, 15, 18, 23, 10],
+      },
+      {
+        label: "Pekan 5",
+        data: [0, 0, 16, 0, 22, 0],
+      },
+    ],
+  },
+  options: {
+    indexAxis: 'y',
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    bar: {
+      borderWidth: 4,
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+    }
+  },
+});
